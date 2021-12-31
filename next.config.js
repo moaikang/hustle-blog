adae9393a9956d8bf1b8c30e3263f00ffda84823
moaikang <moaikang.dev@gmail.com>
2021-12-31 20:25:22 +0900

    meta 태그 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  poweredByHeader: process.env.NODE_ENV === "development",
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,27 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head></Head>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta
+            name="keyword"
+            content="기술 블로그, TIL, 프론트엔드, 자바스크립트, 리액트, 타입스크립트, 회고"
+          />
+          <meta
+            name="description"
+            content="개발자 강근우의 기술 블로그입니다."
+          />
+          <meta name="author" content="moaikang" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>moai.log</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
