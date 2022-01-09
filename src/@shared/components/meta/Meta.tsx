63e47f5d30e5405299aa34a3cbcef963623ebb17
moaikang <moaikang.dev@gmail.com>
2022-01-10 00:23:39 +0900

    meta tag update

--- src/@shared/components/meta/Meta.tsx
+++ src/@shared/components/meta/Meta.tsx
@@ -1,6 +1,11 @@
 import React, { ReactElement } from "react";
 
-function Meta(): ReactElement {
+type Props = {
+  ogTitle?: string;
+  ogDescription?: string;
+};
+
+function Meta({ ogTitle, ogDescription }: Props): ReactElement {
   return (
     <>
       <meta charSet="utf-8" />
@@ -12,13 +17,19 @@ function Meta(): ReactElement {
       <meta name="description" content="개발자 강근우의 기술 블로그입니다." />
       <meta name="author" content="moaikang" />
       <meta property="og:url" content="https://moai.blog" />
-      <meta property="og:title" content="개발자 강근우의 기술 블로그" />
+      <meta
+        property="og:title"
+        content={ogTitle ?? "개발자 강근우의 기술 블로그"}
+      />
       <meta property="og:type" content="website" />
       <meta
         property="og:image"
         content="https://i2.ruliweb.com/img/20/12/24/1769325e6fa148aed.jpg"
       />
-      <meta property="og:description" content="나도 블로그 있다" />
+      <meta
+        property="og:description"
+        content={ogDescription ?? "나도 블로그 만들어따"}
+      />
     </>
   );
 }
