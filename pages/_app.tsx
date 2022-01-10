258841150272c913fc483673c7d49e4ab8539bce
moaikang <moaikang.dev@gmail.com>
2022-01-10 10:32:26 +0900

    공사중 공지 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,13 @@ import type { AppProps } from "next/app";
 import Header from "@shared/components/header";
 import Footer from "@shared/components/footer";
 import ContentWrapper from "@shared/components/content-wrapper";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    window.alert("블로그 공사 중 입니다. 미흡하더라도 양해 부탁드려요 :)");
+  }, []);
+
   return (
     <>
       <Global styles={GlobalStyle} />
