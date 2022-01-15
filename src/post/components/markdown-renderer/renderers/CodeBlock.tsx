e532c6ecbc10fbd01d65800f69ab8dafe336b92a
moaikang <moaikang.dev@gmail.com>
2022-01-15 12:27:11 +0900

    Traffit Light 적용

--- src/post/components/markdown-renderer/renderers/CodeBlock.tsx
+++ src/post/components/markdown-renderer/renderers/CodeBlock.tsx
@@ -1,7 +1,21 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { FunctionComponent } from "react";
 import { CodeProps } from "react-markdown/lib/ast-to-react";
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
+
+const TrafficLight = styled.div`
+  width: 100%;
+  height: 16px;
+  background-color: rgb(30, 30, 30);
+  background-image: url(https://ik.imagekit.io/garbagevalue/garbage/window-buttons_gt8xoXxWn.png);
+  background-repeat: no-repeat;
+  background-size: 44px 10px;
+  border-radius: 12px 12px 0 0;
+  background-position: 1em 1em;
+  padding: 1em;
+  box-sizing: border-box;
+`;
 
 const CodeBlock: FunctionComponent<Omit<CodeProps, "ref">> = ({
   node,
@@ -10,8 +24,8 @@ const CodeBlock: FunctionComponent<Omit<CodeProps, "ref">> = ({
   children,
   ...props
 }) => {
+  const [codeBlockTheme, setCodeBlockTheme] = React.useState();
   const match = /language-(\w+)/.exec(className || "");
-  const [style, setStyle] = React.useState();
 
   React.useEffect(() => {
     if (typeof window !== "undefined") {
@@ -19,19 +33,32 @@ const CodeBlock: FunctionComponent<Omit<CodeProps, "ref">> = ({
         vscDarkPlus,
       } = require("react-syntax-highlighter/dist/esm/styles/prism");
 
-      setStyle(vscDarkPlus);
+      const theme = {
+        ...vscDarkPlus,
+
+        'pre[class*="language-"]': {
+          ...vscDarkPlus['pre[class*="language-"]'],
+          margin: "0",
+          borderRadius: "0 0 12px 12px",
+        },
+      };
+
+      setCodeBlockTheme(theme);
     }
   }, []);
 
   return !inline && match ? (
-    <SyntaxHighlighter
-      style={style}
-      language={match[1]}
-      PreTag="div"
-      {...props}
-    >
-      {String(children).replace(/\n$/, "")}
-    </SyntaxHighlighter>
+    <>
+      <TrafficLight />
+      <SyntaxHighlighter
+        style={codeBlockTheme}
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    </>
   ) : (
     <code className={className} {...props}>
       {children}
